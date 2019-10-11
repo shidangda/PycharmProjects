0724f78dcaca36ad18a1d27fda34f13efebc364a
unknown <2425200635@qq.com>
2019-10-11 21:13:38 +0800

    In library

---
+++ deepLearning/ch1/img.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+img=imread('D:/我的学习资料/Pictures/Saved Pictures/flower.jpg')
+plt.imshow(img)
+
+plt.show()
---
+++ deepLearning/ch2/exercise.py
@@ -0,0 +1,6 @@
+import ch2.perceptron as perceptron
+
+
+print(perceptron.AND(0,1))
+print(perceptron.NAND(0,1))
+print(perceptron.OR(0,1))
